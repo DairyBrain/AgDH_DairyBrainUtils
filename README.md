54cb5186d1fedec79e0c7f97a6d7270151bee1c9
ruipeterpan <rpan0726@gmail.com>
2020-06-24 00:32:59 -0500

    Add license & update setup file

---
+++ README.md
@@ -0,0 +1,8 @@
+# AgDH_database_functions
+
+See [this](https://packaging.python.org/tutorials/packaging-projects/) tutorial for guidance on packaging a Python project and upload it to the PyPI (Python Package Index).
+
+## About
+A PyPI package with some basic functionalities for interacting with a database along with some other Dairy-Brain-specific functionalities.
+
+## Placeholder for an actual README.
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="dairybrain-database-functions-ruipeterpan", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Rui Pan",
+    author_email="rpan33@wisc.edu",
+    description="A set of functions that interacts with a database. It contains some basic functionalities along with some other Dairy-Brain-specific functionalities.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/DairyBrain/AgDH_database_functions",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
